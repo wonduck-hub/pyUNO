b7d602918ebbb39530c36e44d543f30728120b79
ssuzn <tnwls_723@naver.com>
2023-03-24 21:48:10 +0900

    Card 클래스 확장, 카드 앞뒷면 구분, 카드 분배 함수 완료

--- card.py
+++ card.py
@@ -19,26 +19,25 @@ class Card:
         self.value = value
         self.width = 100
         self.height = 150  
+        self.face_up = False # 카드 뒤집어져 있는지 여부, 기본값 = back
         
         image_path = f"card_image/{color}_{value}.png"
         self.image = pygame.transform.scale(pygame.image.load(image_path), (self.width, self.height))
         
         self.rect = self.image.get_rect() # 카드의 사각형 영역
-
+        
     def draw(self, x, y): # 위치 정보 인자로 받아 카드 그리기
+      if self.face_up:
         screen.blit(self.image, (x, y))
-
-    def is_clicked(self, x, y): # 카드 선택되었는지 확인
-      if self.x <= x <= self.x + self.width and self.y <= y <= self.y + self.height:
-        return True
       else:
-        return False
-      
-    def select(self): # 카드 선택한 상태로 변경
-       pygame.draw.rect(screen, white, (self.rect, self.width, self.height), 5)
-      
-    def deselect(self): # 카드 선택 해제 상태로 변경
-      pygame.draw.rect(screen, )
+        self.image = pygame.transform.scale(pygame.image.load("card_image/back.png"), (self.width, self.height))
+        screen.blit(self.image, (x, y))
+        
+    # def isClicked(self, pos):
+    #   return self.rect.collidepoint(pos)
+
+    def flip(self): # 카드 앞면 뒷면 전환
+      self.face_up = not self.face_up
       
       
 class NumberCard(Card):
@@ -56,7 +55,8 @@ class Deck:
     def __init__(self):
         self.cards = [] # 카드 덱
         self.createDeck() # 카드 덱 초기화
-
+        self.selectedCard = None
+        
     def createDeck(self):
         self.cards = [] # cards 빈 리스트로 초기화
         # 카드 생성하여 cards에 추가
@@ -67,8 +67,8 @@ class Deck:
         for i in range(4):
             for value in noncolor_values: # 색 없는 기술카드 4장
                 self.cards.append(Card("None", value))
+                
         return self.cards
-
         
     def prepareCard(self): # 게임 시작 전 플레이어에게 card 분배 및 카드더미 섞기
       handsOnCard = [] # 플레이어가 들고 있는 카드
@@ -96,7 +96,7 @@ pygame.display.set_caption("카드 게임")
 
 pygame.display.update()
 
-
+    
 running = True
 while running:
     for event in pygame.event.get():
@@ -106,16 +106,19 @@ while running:
 
     bg_color = pygame.Color("green")
     screen.fill(bg_color)
+    pos = pygame.mouse.get_pos()
     
+    # 예시코드
     # 숫자 카드 객체 생성
-    # number_card = NumberCard("red", 1)
+    #number_card = NumberCard("blue", 1)
     # 특수 능력 카드 객체 생성
-    # ability_card = AbilityCard("None", "joker")
-    # ability_card1 = AbilityCard("green", "skip")
+    #ability_card = AbilityCard("None", "joker")
+    #ability_card1 = AbilityCard("green", "skip")
     # 카드 그리기
-    # number_card.draw(50, 50)
-    # ability_card.draw(100, 100)
-    # ability_card1.draw(200, 200)
+    #number_card.draw(50, 50)
+    #ability_card.draw(100, 100)
+    #ability_card1.flip()
+    #ability_card1.draw(200, 200)
     
     # 화면 업데이트
     pygame.display.update()
